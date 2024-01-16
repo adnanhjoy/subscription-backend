c1249730cf3aa670890a051fa548e55d8490f73b
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-16 16:24:16 +0600

    get package

--- src/controller/packageController.js
+++ src/controller/packageController.js
@@ -25,5 +25,28 @@ packageRouter.post('/', async (req, res) => {
 });
 
 
+// get package data 
+
+packageRouter.get('/', async (req, res) => {
+    try {
+        const result = await Package.find();
+        if (result) {
+            res.status(200).json({
+                success: true,
+                data: result,
+            })
+        } else {
+            res.status(404).json({
+                message: 'package not found'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+
 
 module.exports = packageRouter;
