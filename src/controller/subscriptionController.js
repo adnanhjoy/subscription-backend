5ea5315ddd3769317767e1b8e93048cf90b37379
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-16 13:53:14 +0600

    add signin controller

--- src/controller/subscriptionController.js
+++ src/controller/subscriptionController.js
@@ -2,9 +2,12 @@ const express = require('express');
 const SubscriptionUser = require('../model/subscriptionSchema');
 const subscriptionRouter = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
-subscriptionRouter.post('/user', async (req, res) => {
+
+// signup subscription user 
+subscriptionRouter.post('/user/signup', async (req, res) => {
     const { name, email, password, phone, country, city, address } = req.body
     try {
         const isExist = await SubscriptionUser.findOne({ email: email });
@@ -44,11 +47,43 @@ subscriptionRouter.post('/user', async (req, res) => {
 
 
 
-// subscriptionRouter.get('/', async (req, res) => {
-//     res.json({
-//         name: 'Adnan Hossain'
-//     })
-// })
+// login subscription user
+subscriptionRouter.post('/user/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const isExistUser = await SubscriptionUser.find({ email: email });
+        if (isExistUser) {
+            const isValidPassword = await bcrypt.compare(password, isExistUser[0].password);
+            if (isValidPassword) {
+                let token = jwt.sign({
+                    name: isExistUser[0].name,
+                    email: isExistUser[0].email
+                }, process.env.PRIVATE_KEY, { expiresIn: '1h' });
+
+                res.status(200).json({
+                    access_token: token,
+                    user: {
+                        name: isExistUser[0].name,
+                        email: isExistUser[0].email
+                    }
+                })
+            } else {
+                res.status(404).json({
+                    message: 'Password incorrect'
+                });
+            }
+        } else {
+            res.status(404).json({
+                message: 'Email not valid'
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+})
 
 module.exports = {
     subscriptionRouter
